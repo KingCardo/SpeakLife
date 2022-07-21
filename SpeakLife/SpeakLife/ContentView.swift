7bf9c1467d38347314d217421c9978863f430867
Riccardo Washington <kingcardo@icloud.com>
2022-07-20 23:04:44 -0600

    proj  setup

---
+++ SpeakLife/SpeakLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpeakLife
+//
+//  Created by Riccardo Washington on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpeakLife/SpeakLife/SpeakLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakLifeApp.swift
+//  SpeakLife
+//
+//  Created by Riccardo Washington on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeakLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
